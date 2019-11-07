18d47e92cda227738bc6dcc5581c43754e91b593
Kizito Njoku <nkizito57@gmail.com>
2019-11-07 19:31:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+# def my_collect(array) 
+#   count = 1 
+#   collect = []
+#   while count < array.length 
+#     collect << "#{array}"
+#   yield(array) 
+#     collect
+#   end
+# end 
+array["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end 
