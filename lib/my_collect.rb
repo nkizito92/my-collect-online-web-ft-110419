dac1d1c02b2c22d779dd45609edf22cc0a4efeea
Kizito Njoku <nkizito57@gmail.com>
2019-11-07 19:36:57 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array) 
+  count = 0
+  collection = []
+  while count < array.length 
+    collection << yield(array[count]) 
+    count += 1
+  end
+  collection
+end 
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'pythn', 'objective-c']
 
 
+my_collect(array) do |name|
+  name.split(" ").first
+end 
+
+my_collect(collection) do |lang|
+  lang.upcase
+end 
